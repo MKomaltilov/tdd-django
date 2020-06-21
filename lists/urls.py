105d4ccf5100ca97f165a05be6857a1c35f6db8f
Marat Komaltilov <marat.komaltilov@icloud.com>
2020-06-21 19:27:33 +0700

    list page added, unittests updated

--- lists/urls.py
+++ lists/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home_page, name='home')
+    path('', views.home_page, name='home'),
+    path('lists/very-unique-list/', views.view_list, name='view_list')
 ]
